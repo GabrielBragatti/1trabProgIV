0758b028e19369f206ef115bb042eb19d3682452
GabrielBragatti <gabriel.bragatti@gmail.com>
2024-02-22 20:54:40 -0300

    Esqueci de commitar no desenvolvimento ;-;

---
+++ Umfg.Exercicio03.ProgIV/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Umfg.Exercicio03.ProgIV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a distancia percorrida em quilometros: ");
+            string km = Console.ReadLine();
+            Console.WriteLine("Informe o seu gasto de combustivel em litros: ");
+            string combustivel = Console.ReadLine();
+        }
+    }
+}
